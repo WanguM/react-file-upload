2db77022cb6b222e60d6f18ff6770bc0341341a9
WanguM <susanwangu@gmail.com>
2018-12-13 00:15:27 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageUploader from "react-images-upload";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(pictureFiles, pictureDataURLs) {
+    this.setState({
+      pictures: this.state.pictures.concat(pictureFiles)
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ImageUploader
+        withIcon={true}
+        buttonText="Choose images"
+        onChange={this.onDrop}
+        imgExtension={[".jpg", ".png"]}
+        maxFileSize={5242880}
+        withPreview={true}
+        withLabel={true}
+        label="Max file size is 5 mb"
+      />
     );
   }
 }
